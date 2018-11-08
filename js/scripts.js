5c582549fd87df4d810925774ec1695cd3eacf13
johnteruya <jteruya9@gmail.com>
2018-11-08 17:55:33 -0200

    incluindo funções js

--- js/scripts.js
+++ js/scripts.js
@@ -1,15 +1,42 @@
-//MÉTODO DE INCLUSÃO
-//FUNÇÃO INCLUIR
-var botaoAdicionar = document.querySelector("#adicionar-pessoas");
+//FUNÇÃO DO BOTÃO
+var botaoAdicionar = document.querySelector("#incluir-pessoas");
 botaoAdicionar.addEventListener("click", function(event){
     event.preventDeafult();
-    var form = document.querySelector("#form-adiciona");
-    var pessoa = obtemPessoa(form);
+    var form = document.querySelector("#form-incluir");
+    var pessoa = obtemPessoaFormulario(form);
     incluirPessoas(pessoa);
+    form.reset();
 });
 
-//FUNÇÃO 
-function incluirPessoas(pessoas){
+//FUNÇÃO QUE OBTEM DADOS
+function obtemPessoaFormulario(form){
+     var pessoa = {
+         nome: form.nome.value,
+         rg: form.rg.value,
+         contato: form.contato.value,
+         local: form.local.value
+     }
+     return pessoa;
+}
+
+function montaTr(pessoa){
+    var pessoaTr=document.createElement("tr");
+    pessoaTr.classList.add("pessoa");
+
+    pessoaTr.appendChild(montaTd(pessoa.nome, "info-nome"));
+    pessoaTr.appendChild(montaTd(pessoa.rg, "info-nome"));
+    pessoaTr.appendChild(montaTd(pessoa.contato, "info-nome"));
+    pessoaTr.appendChild(montaTd(pessoa.local, "info-nome"));
+}
+
+function montaTd(){
+    var td = document.createElement("td");
+    td.classList.add(classe);
+    td.textContent = dado;
+    return td;
+}
+
+function incluirPessoas(pessoa){
     var pessoaTr = montaTr(pessoa);
     var tabela = document.querySelector("#tabela-pessoas");
     tabela.appendChild(pessoaTr);

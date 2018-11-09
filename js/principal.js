f144574148924b7f69540c567e35e7f28537f7ce
johnteruya <jteruya9@gmail.com>
2018-11-09 12:09:54 -0200

    metade aula 3 concluida

--- js/principal.js
+++ js/principal.js
@@ -13,3 +13,38 @@ var altura = tdAltura.textContent;
 var imc = peso / (altura * altura);
 var tdImc = paciente.querySelector(".info-imc");
 tdImc.textContent = imc;
+
+
+var pacientes = document.querySelectorAll(".paciente");
+
+    for (var i =0; i < pacientes.length; i++){
+        var paciente = pacientes[i];
+
+        var tdPeso = document.querySelector(".info-peso");
+        var tdAltura = document.querySelector(".info-altura");
+        var tdImc = document.querySelector(".info-imc");
+
+        var peso = tdPeso.textContent;
+        var altura = tdAltura.textContent;
+
+        var pesoValido = true;
+        var alturaValida = true;
+
+            if (peso <= 0 || peso > 1000){
+                console.log("PESO INVÁLIDO!");
+                tdImc.textContent = "PESO INVALIDO!";
+                pesoValido = false;
+            }
+            
+            if (altura <= 0 || altura > 1000){
+                console.log("ALTURA INVÁLIDA!");
+                tdImc.textContent ="PESO INVÁLIDO!";
+                alturaValida = false;
+            }
+
+            if(pesoValido && alturaValida){
+                var imc = peso / (altura * altura);
+                tdImc.textContent = imc.toFixed(2);
+            }
+
+    }

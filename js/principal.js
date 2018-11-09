a6352cf30e002cfde18c430a2a077cbdf0d42455
johnteruya <jteruya9@gmail.com>
2018-11-09 16:42:03 -0200

    finalizando aula 4

--- js/principal.js
+++ js/principal.js
@@ -34,16 +34,52 @@ for(var i = 0; i < pacientes.length ; i++){
         console.log("Peso inválido");
         tdImc.textContent = "Peso inválido!";
         pesoEhValido = false;
+
+        paciente.classList.add("paciente-invalido");
     }
 
     if(altura <= 0 || altura >= 1000){
         console.log("Altura inválida");
         tdImc.textContent = "Altura inválida!";
         alturaEhValida = false;
+
+        paciente.classList.add("paciente-invalido");
     }
 
     if(pesoEhValido && alturaEhValida){
         var imc = peso / ( altura * altura);    
         tdImc.textContent = imc.toFixed(2);
-    }    
+    }
 }
+
+    var botaoAdicionar = document.querySelector("#adicionar-paciente");
+    botaoAdicionar.addEventListener("click", function() {
+        event.preventDefault();
+
+        var form = document.querySelector("#form-adiciona");
+
+        var nome = form.nome.value;
+        var peso = form.peso.value;
+        var altura = form.altura.value;
+        var gordura = form.gordura.value;
+
+        var pacienteTr = document.createElement("tr");
+
+        var nomeTd = document.createElement("td");
+        var pesoTd = document.createElement("td");
+        var alturaTd = document.createElement("td");
+        var gorduraTd = document.createElement("td");
+
+        nomeTd.textContent = nome;
+        pesoTd.textContent = peso;
+        alturaTd.textContent = altura;
+        gorduraTd.textContent = gordura;
+
+        pacienteTr.appendChild(nomeTd);
+        pacienteTr.appendChild(pesoTd);
+        pacienteTr.appendChild(alturaTd);
+        pacienteTr.appendChild(gorduraTd);
+
+        var tabela = document.querySelector("#tabela-pacientes");
+        tabela.appendChild(pacienteTr);
+    });

483067c9b507700288e9b3f3642bfffc649724d8
johnteruya <jteruya9@gmail.com>
2018-11-29 11:05:58 -0200

    ok

--- js/scripts.js
+++ js/scripts.js
@@ -1,32 +1,44 @@
 //MANIPULANDO ELEMENTOS DO HTML PELO JS
 var titulo = document.querySelector(".titulo");
-titulo.textContent="Nome da Tabela";
+titulo.textContent="Planejamento Financeiro";
 
-//CRIANDO BOTÃO DE INSERÇÃO
-var botaoAdicionar = document.querySelector("#adicionar-nome");
-
+//INSERINDO ELEMENTOS NA TABELA
+var botaoAdicionar = document.querySelector("#adicionar-gastos");
 botaoAdicionar.addEventListener("click", function(event){
     event.preventDefault();
     
+    var form = document.querySelector("#formulario");
 
+    var gastos = form.gastos.value;
+    var valor = form.valor.value;
 
+    var gastosTr = document.createElement("tr");
+    var valorTr = document.createElement("tr");
 
+    var gastosTd = document.createElement("td");
+    var valorTd = document.createElement("td");
 
+    gastosTd.textContent = gastos;
+    valorTd.textContent = valor;
+    
+    gastosTr.appendChild(gastosTd);
+    valorTr.appendChild(valorTd);
+    
+    var tabela = document.querySelector("#tabela");
 
-
-
-
-
+    tabela.appendChild(gastosTr);
+    tabela.appendChild(valorTr);
+    
+    form.reset();
 });
 
-//VERIFICADOR DE CAMPO NULO
-function required(){
-    var vazio = document.forms["form"]["nome"].value;
-        if (vazio == ""){
-            alert("Por Favor insera um valor");
-            return false;
-        } else {
-            alert('OK!');
-            return true; 
-        }
-}
+//BOTAO TESTE
+var botao = document.querySelector("#botao");
+botao.addEventListener("click", function(){
+    event.preventDefault();
+
+    var add = document.querySelector("#add");
+    var addSpan = document.createElement("span");
+    addSpan.textContent = add;
+    add.appendChild();
+});
